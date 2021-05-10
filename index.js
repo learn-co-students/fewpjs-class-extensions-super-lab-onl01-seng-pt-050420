782049b92784b8d52b91248b3443644652060504
Pwako11 <akolo@hotmail.com>
2021-05-10 12:24:48 -0400

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Tree {
     }
 
     static definition(){
-        return 'A tree is a perenial plant with an elongated stem, or trunk, supporting branches and leaves.'
+        return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.'
     } 
 }
 
